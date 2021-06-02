12875130e661daca4d9fb361eed9be556beb8ffa
Alex <maralezo@gmail.com>
2021-06-02 23:54:11 +0300

    Refactor bot to be one instance while listening; Add logger

--- bot_service.rb
+++ bot_service.rb
@@ -1,47 +1,50 @@
 require 'dotenv/load'
-require 'telegram/bot'
+require 'logger'
 require 'pry'
+require 'telegram/bot'
 require_relative 'db_controller'
 
-module TeleBot
+module TelegramService
+  BOT = Telegram::Bot::Client.new(ENV['T_TOKEN'])
   class << self
-    def bot
-      Telegram::Bot::Client.new(ENV['T_TOKEN'])
-    end
     def listen
-      bot.run do |current_bot|
+      logger = Logger.new(STDOUT)
+      BOT.run do |current_bot|
         current_bot.listen do |message|
-          # binding.pry
+          binding.pry
           case message
           when Telegram::Bot::Types::ChatMemberUpdated
             user_status = message.new_chat_member.status
-            puts user_status
-            if user_status == 'member'
+            # BOT.api.get_chat_member(chat_id: message.chat.id, user_id: message.from.id)
+            case user_status
+            when 'member'
               DB.insert User.new(name: message.chat.username, chat_id: message.chat.id, timestamp: Time.now.to_s)
-            else
-              DB.unsubscribe message.chat.username
+              logger.info "User: #{message.from.username}(#{message.chat.id}) is subscribed!"
+            when 'kicked'
+              DB.unsubscribe message.chat.id
+              logger.info "User: #{message.from.username}(#{message.chat.id}) is unsubscribed!"
             end
           when Telegram::Bot::Types::Message
-            bot.api.send_message(chat_id: message.chat.id, text: time_left) if message.text == '/start'
+            BOT.api.send_message(chat_id: message.chat.id, text: time_left) if message.text == '/start'
           end
         end
       end
     end
 
     def time_left
-      games = DB.get
+      games = DB.games
+      # binding.pry
+      if games.empty?
+        puts 'Следующая раздача неизвестна!'
+        return
+      end
       game = games.first
       days_in_sec = (Time.parse(game.end_date) - Time.now).to_i
       days, hours_in_sec = days_in_sec.divmod(60 * 60 * 24)
       hours, minutes_in_sec = hours_in_sec.divmod(60 * 60)
       minutes, seconds = minutes_in_sec.divmod(60)
       "Вы подписаны!\n" \
-      "Следующая раздача через: #{days} д., #{hours} ч., #{minutes} м., #{seconds} с."
-    end
-
-    def mini_logger(current_message)
-      puts "Username: #{current_message.from.username}, Command: #{current_message.text}"
-      puts "Date: #{Time.at(current_message.date)}"
+      "Следующая раздача через: #{days} дн.: #{hours} ч.: #{minutes} м.: #{seconds} с."
     end
   end
 end
