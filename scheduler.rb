a24930125d8a8a12d33296ca009fa0043c89cfb2
Alexey K <alexcrpts@gmail.com>
2021-07-22 22:41:43 +0300

    Refactor Scheduler.

--- scheduler.rb
+++ scheduler.rb
@@ -4,86 +4,131 @@ require_relative 'models'
 require_relative 'egscraper'
 require_relative 'template'
 
-module Schedule
-  class << self
-    def plan
-      Thread.new do
-        logger = Logger.new($stdout)
-        loop do
-          games = parse
-          if games.empty?
-            logger.info 'Games returned nothing! Skipping...'
-          else
-            store(games)
-            logger.info 'Games have been successfully saved to Database!'
-          end
-          chat_ids = User.chat_ids
-          if chat_ids.empty?
-            logger.info 'No subscribed users! Skipping...'
-          else
-            dispatch(games.count, chat_ids, logger)
-          end
-          wait
+class Schedule
+  attr_reader :logger, :games
+
+  def initialize
+    @logger = Logger.new($stdout)
+    @games = []
+  end
+
+  def plan
+    Thread.new do
+      loop do
+        next_date = FreeGame.next_date
+        # binding.pry
+        if behind?(next_date)
+          logger.info 'Skipping to the next release!'
+          wait(next_date)
+        else
+          serve_games_to_users
+          # serve_games
+          # serve_users
+          # binding.pry
+          wait(FreeGame.next_date)
         end
       end
     end
+  end
 
-    private
+  private
 
-    def parse
-      5.times do
-        promotions = Parser::Promotions.run
-        return promotions unless promotions.empty?
+  def serve_games_to_users
+    @games = parse_games
+    chat_ids = User.chat_ids
+    return logger.info 'Games returned nothing! Skipping...' if games.empty?
+    return logger.info 'No subscribed users! Skipping...' if chat_ids.empty?
 
-        sleep rand(20..30)
+    store(games)
+    logger.info 'Games have been successfully saved to Database!'
+    dispatch(games.count, chat_ids)
+  end
+
+  # def serve_games
+  #   @games = parse_games
+  #   binding.pry
+  #   if games.empty?
+  #     logger.info 'Games returned nothing! Skipping...'
+  #   else
+  #     # binding.pry
+  #     store(games)
+  #     logger.info 'Games have been successfully saved to Database!'
+  #   end
+  # end
+
+  def parse_games
+    5.times do
+      promotions = Parser::Promotions.run
+      return promotions unless promotions.empty?
+
+      wait
+    end
+    []
+  end
+
+  def store(games)
+    games.each do |game|
+      FreeGame.new(game).save
+    end
+  end
+
+  # def serve_users
+  #   # binding.pry
+  #   chat_ids = User.chat_ids
+  #   if chat_ids.empty?
+  #     logger.info 'No subscribed users! Skipping...'
+  #   elsif !games.empty?
+  #     dispatch(games.count, chat_ids)
+  #   end
+  # end
+
+  def dispatch(count, chat_ids)
+    games = FreeGame.games(count)
+
+    chat_ids.each do |chat_id|
+      TelegramService::BOT.api.send_message(chat_id: chat_id, text: Template.new(games), parse_mode: 'html')
+      logger.info "Games have been dispatched to #{chat_id}!"
+
+    # Telegram::Bot::Exceptions::ResponseError
+    # Telegram API has returned the error. (ok: "false", error_code: "403",
+    # description: "Forbidden: bot was blocked by the user")
+
+    rescue => exception 
+      logger.error exception.message
+
+      if process(exception)[:error_code] == '403'
+        logger.info "Invalid user(#{chat_id}). Unsubscribing..."
+        User.unsubscribe chat_id
       end
-      []
+
+      next
     end
+  end
 
-    def store(games)
-      games.each do |game|
-        FreeGame.new(game).save
-      end
-    end
+  def process(exception)
+    message = exception.message.match(/(?<=\().+(?=\))/).to_s
+    message
+      .split(',')
+      .to_h { |k_v_pair| [k_v_pair[/\w+[^:]/], k_v_pair[/(?<=\").+[^"]/]] }
+      .transform_keys(&:to_sym)
+  end
 
-    def dispatch(count, chat_ids, logger)
-      games = FreeGame.games(count)
+  def wait(date = nil)
+    that_much = case date
+                when nil
+                  60 * 5
+                when 'day'
+                  60 * 60 * 24
+                when Time
+                  date - Time.now
+                end
+    # binding.pry
+    sleep that_much
+  end
 
-      chat_ids.each do |chat_id|
-        TelegramService::BOT.api.send_message(chat_id: chat_id, text: Template.new(games), parse_mode: 'html')
-        logger.info "Games have been dispatched to #{chat_id}!"
+  def behind?(date)
+    return false if date.nil?
 
-      # Telegram::Bot::Exceptions::ResponseError
-      # Telegram API has returned the error. (ok: "false", error_code: "403",
-      # description: "Forbidden: bot was blocked by the user")
-
-      rescue => exception 
-        logger.error exception.message
-
-        if process(exception)[:error_code] == '403'
-          logger.info "Invalid user(#{chat_id}). Unsubscribing..."
-          User.unsubscribe chat_id
-        end
-
-        next
-      end
-    end
-
-    def process(exception)
-      message = exception.message.match(/(?<=\().+(?=\))/).to_s
-      message
-        .split(',')
-        .to_h { |k_v_pair| [k_v_pair[/\w+[^:]/], k_v_pair[/(?<=\").+[^"]/]] }
-        .transform_keys(&:to_sym)
-    end
-
-    def wait
-      next_date = FreeGame.next_date
-      day = 60 * 60 * 24
-      return sleep day if next_date.nil?
-
-      next_date -= Time.now
-      sleep next_date.negative? ? day : next_date
-    end
+    (date - Time.now).positive?
   end
 end
