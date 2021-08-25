ed447cd779ae68c02557c2a40897622ec64c385f
Alexey K <alexcrpts@gmail.com>
2021-08-25 19:50:36 +0300

    scheduler: Clean up instance variables.

--- scheduler.rb
+++ scheduler.rb
@@ -5,18 +5,16 @@ require_relative 'egscraper'
 require_relative 'template'
 
 class Schedule
-  attr_reader :logger, :games
+  attr_reader :logger
 
   def initialize
     @logger = Logger.new($stdout)
-    @games = []
   end
 
   def plan
     Thread.new do
       loop do
         next_date = FreeGame.next_date
-        # binding.pry
         if behind?(next_date)
           logger.info 'Skipping to the next release!'
           wait(next_date)
@@ -31,7 +29,7 @@ class Schedule
   private
 
   def serve_games_to_users
-    @games = parse_games
+    games = parse_games
     chat_ids = User.chat_ids
     return logger.info 'Games returned nothing! Skipping...' if games.empty?
     return logger.info 'No subscribed users! Skipping...' if chat_ids.empty?
