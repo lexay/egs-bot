8fc9ca91f3b52ec1b87c620b79e6ad1e66914521
Alexey K <alexcrpts@gmail.com>
2021-09-27 23:52:31 +0300

    Why do we actually need this?

--- template.rb
+++ template.rb
@@ -1,33 +1,31 @@
 module Template
-  class << self
-    def new(games)
-      a_game = games.first
-      text = "Текущая раздача от ЕГС с #{stringify a_game.start_date} по #{stringify a_game.end_date}:\n\n"
+  def new(games)
+    a_game = games.first
+    text = "Текущая раздача от ЕГС с #{stringify a_game.start_date} по #{stringify a_game.end_date}:\n\n"
 
-      show_idx = ->(idx) { format('%i. ', idx + 1) }
-      show_no_idx = proc { '' }
-      game_idx = games.count > 1 ? show_idx : show_no_idx
-      games.each_with_index do |game, idx|
-        message = <<~MESSAGE
-          <strong>Название:</strong> <a href='#{game.game_uri}'>#{game.title}</a>
+    show_idx = ->(idx) { format('%i. ', idx + 1) }
+    show_no_idx = proc { '' }
+    game_idx = games.count > 1 ? show_idx : show_no_idx
+    games.each_with_index do |game, idx|
+      message = <<~MESSAGE
+        <strong>Название:</strong> <a href='#{game.game_uri}'>#{game.title}</a>
 
-          <strong>Издатель / Разработчик:</strong> #{game.pubs_n_devs}
+        <strong>Издатель / Разработчик:</strong> #{game.pubs_n_devs}
 
-          <strong>Описание:</strong>
-          #{game.short_description.length < 10 ? game.full_description : game.short_description}
-          <a href='#{game.game_uri}'>...</a>
+        <strong>Описание:</strong>
+        #{game.short_description.length < 10 ? game.full_description : game.short_description}
+        <a href='#{game.game_uri}'>...</a>
 
-        MESSAGE
-        text << game_idx.call(idx) << message
-      end
-      text
+      MESSAGE
+      text << game_idx.call(idx) << message
     end
+    text
+  end
 
-    def stringify(date)
-      day = date.strftime('%-d')
-      month_idx = date.strftime('%m').to_i
-      month = %w[января февраля марта апреля мая июня июля августа сентября октября ноября декабря][month_idx - 1]
-      "#{day} #{month}"
-    end
+  def stringify(date)
+    day = date.strftime('%-d')
+    month_idx = date.strftime('%m').to_i
+    month = %w[января февраля марта апреля мая июня июля августа сентября октября ноября декабря][month_idx - 1]
+    "#{day} #{month}"
   end
 end
