6f3fd86a071c2c06320c01d7f1f003175cff4ac1
Alexey K <alexcrpts@gmail.com>
2021-10-11 12:31:35 +0300

    Add migrations. Move models.

---
+++ db/migrations/1_create_free_game.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  change do
+    create_table(:free_games) do
+      primary_key :id
+      String :title, :size=>255
+      String :full_description, text: true
+      String :short_description, text: true
+      String :pubs_n_devs, :size=>255
+      String :game_uri, :size=>255
+      DateTime :start_date
+      DateTime :end_date
+      DateTime :timestamp
+    end
+  end
+end
