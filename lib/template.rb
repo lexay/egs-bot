5445fc11c74792ea31f75f1fcb0d2f0f361eaa54
Alexey K <alexcrpts@gmail.com>
2021-10-16 16:49:30 +0300

    Return empty string if games are empty.

--- lib/template.rb
+++ lib/template.rb
@@ -1,6 +1,8 @@
 module EGS
   module Template
     def self.new(games)
+      return '' if games.empty?
+
       a_game = games.first
       text = "Текущая раздача от ЕГС с #{stringify a_game.start_date} по #{stringify a_game.end_date}:\n\n"
 
