f985d217d8965a72808dce6cf36b5e6ff65caca8
Alexey K <alexcrpts@gmail.com>
2021-10-28 23:13:37 +0300

    Fix Scheduler starting 1 second earlier than it should, due to rounding down to lowest value.

--- helpers/time_helper.rb
+++ helpers/time_helper.rb
@@ -3,7 +3,7 @@ module TimeHelper
     next_date = EGS::Models::FreeGame.next_date
     return -1 if next_date.nil? 
 
-    (next_date - Time.now).to_i
+    (next_date - Time.now).round
   end
 
   def release_date_ahead?
@@ -13,6 +13,7 @@ module TimeHelper
   def seconds_to_human_readable(time_in_seconds)
     return 'Неизвестно!' if time_in_seconds.negative?
 
+    EGS::LOG.info time_in_seconds
     days, hours_in_sec = time_in_seconds.divmod(60 * 60 * 24)
     hours, minutes_in_sec = hours_in_sec.divmod(60 * 60)
     minutes, seconds = minutes_in_sec.divmod(60)
