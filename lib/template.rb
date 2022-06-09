577efd693f021a310ab1c852b78c3bd5375684f3
Alexey K <alexcrpts@gmail.com>
2022-06-09 17:37:12 +0300

    fix(template): add help message for banned
    * Add info how to bypass ban for russians
    * Refactor Template

--- lib/template.rb
+++ lib/template.rb
@@ -12,19 +12,9 @@ module EGS
         header = "Текущая раздача от ЕГС с #{stringify game.start_date} по #{stringify game.end_date}:\n\n"
         header = '' if expiration_date == game.end_date
         expiration_date = game.end_date
-
-        message = <<~MESSAGE
-          <strong>Название:</strong> <a href='#{game.game_uri}'>#{game.title}</a>
-
-          <strong>Издатель / Разработчик:</strong> #{game.pubs_n_devs}
-
-          <strong>Описание:</strong>
-          #{game.description.truncate(300, separator: '.')}
-
-        MESSAGE
         info << header << game_idx.call(idx) << message
       end
-      info
+      info << banned_message
     end
 
     def self.stringify(date)
@@ -32,6 +22,28 @@ module EGS
       month_idx = date.strftime('%m').to_i
       month = %w[января февраля марта апреля мая июня июля августа сентября октября ноября декабря][month_idx - 1]
       "#{day} #{month}"
+    end
+
+    def self.message(game)
+      <<~MESSAGE
+        <strong>Название:</strong> <a href='#{game.game_uri}'>#{game.title}</a>
+
+        <strong>Издатель / Разработчик:</strong> #{game.pubs_n_devs}
+
+        <strong>Описание:</strong>
+        #{game.description.truncate(300, separator: '.')}
+
+      MESSAGE
+    end
+
+    def self.banned_message
+      <<~MESSAGE
+
+
+      Как забирать с ЕГС из России описано по ссылке ниже:
+
+      https://dtf.ru/flood/1194376-kak-zabirat-nedostupnye-igry-v-rossii-s-razdach-egs
+      MESSAGE
     end
   end
 end
