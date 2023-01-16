b17698b5077f7c450c951e54026e23db498199a7
Alexey K <alexcrpts@gmail.com>
2023-01-16 21:48:06 +0300

    collect discount_price more reliably

--- helpers/scraper_helper.rb
+++ helpers/scraper_helper.rb
@@ -1,14 +1,14 @@
 module ScraperHelper
   include Hashie::Extensions::DeepFind
 
-  def no_api?
-    self[:product_slug].nil?
+  def api?
+    !self[:product_slug].nil?
   end
 
   def current_and_free?
     current_promo = self.dig(:promotions, :promotional_offers)
     return false if current_promo.nil? || current_promo.empty?
 
-    self.deep_find_all(:discount_price).any?(0)
+    self.deep_find(:discount_price).to_f.zero?
   end
 end
