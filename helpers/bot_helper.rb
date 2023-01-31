f376c5dedd7907c0db8d98efaf9c61cf9de22af0
Alexey K <alexcrpts@gmail.com>
2023-01-31 18:13:52 +0300

    clean up code
    * Dont extract methods into mixins if they are not used elsewhere but
      one class.
    
    * Dont use top context in namespace resolution, it is implied.
    
    * Move invocation on Release.new if Release.last is nil to Models, where
      it belongs.

--- helpers/bot_helper.rb
+++
@@ -1,5 +0,0 @@
-module BotHelper
-  def send_message(text, chat_id)
-    EGS::TG_BOT.api.send_message(chat_id: chat_id, text: text, parse_mode: 'html')
-  end
-end
--- helpers/game_helper.rb
+++
@@ -1,9 +0,0 @@
-module GameHelper
-  def query_release
-    EGS::Models::Release.last || EGS::Models::Release.new
-  end
-
-  def format(games)
-    games.empty? ? I18n.t(:release_unknown) : EGS::Template.new(games)
-  end
-end
