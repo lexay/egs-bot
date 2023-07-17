f533aa72afa47463ab5e16e5f894ed47c1dbc19e
Alexey K <alexcrpts@gmail.com>
2023-07-17 17:44:58 +0300

    clean up initialization file
    * Dont assign the DB connection to a constant, it will not be reused.
    
    * Move down the BOT constant to where it's estethically pleasing to the OCD eye.
      Specifically to the special bottom section where all the constants reside ;-)
    
    * Add `db/models/models.rb` in the array of all the application related files that required by
      `require_relative`. Why is it being required on its own separate line? Not fair! :-)

--- start.rb
+++ start.rb
@@ -3,16 +3,16 @@ require 'bundler'
 Bundler.setup(:default)
 Bundler.require(:default)
 system('rake', 'db:migrate')
+
 module EGS
-  BOT = Telegram::Bot::Client.new(ENV['TG_TOKEN'])
-  DB = Sequel.connect(ENV['DATABASE_URL'])
+  Sequel.connect(ENV['DATABASE_URL'])
   Sequel.default_timezone = :utc
   I18n.load_path << Dir[File.expand_path('config/locales') + '/*.yml']
   I18n.default_locale = :ru
+  BOT = Telegram::Bot::Client.new(ENV['TG_TOKEN'])
   LOG = Logger.new($stdout)
 end
 
-require_relative 'db/models/models'
-Dir['./helpers/*.rb', './lib/*.rb'].each { |f| require_relative f }
+Dir['db/models/models.rb', './helpers/*.rb', './lib/*.rb'].each { |f| require_relative f }
 
 EGS::Schedule.new.run
