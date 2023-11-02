6f47fd41918bb85c368e82c8621d144371d724d7
Alexey K <alexcrpts@gmail.com>
2023-11-03 00:35:48 +0300

    translate logs
    * Make `time_helper`'s `convert_to_human_readable` do one thing only.
    * Make `Notifier#push` do one thing only.
    * Delegate logging to `Scheduler`.

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -2,7 +2,6 @@ module EGS
   class Notifier
     def self.push(text, chat_id: ENV['TG_CHANNEL'])
       BOT.api.send_message(text:, chat_id:, parse_mode: 'html')
-      LOG.info 'Games have been dispatched to the channel!'
     end
   end
 end
