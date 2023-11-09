a856f16cf8eeacdee166424b3ba1ce4a011a1131
Alexey K <alexcrpts@gmail.com>
2023-11-09 10:55:53 +0300

    let linter do its (evil) thing
    * No need to be super expressive, because `self` is implied when there
      is no receiver in method calls. It is helpful at the beginner level
      though.

--- db/models/models.rb
+++ db/models/models.rb
@@ -4,19 +4,19 @@ module EGS
       many_to_one :release
 
       def eql?(other)
-        self.title == other.title &&
-          self.start_date == other.start_date &&
-          self.end_date == other.end_date
+        title == other.title &&
+          start_date == other.start_date &&
+          end_date == other.end_date
       end
 
       def hash
-        self.title.hash ^
-          self.start_date.hash ^
-          self.end_date.hash
+        title.hash ^
+          start_date.hash ^
+          end_date.hash
       end
 
       def <=>(other)
-        self.end_date <=> other.end_date
+        end_date <=> other.end_date
       end
 
       alias == eql?
@@ -26,11 +26,11 @@ module EGS
       one_to_many :free_games
 
       def self.last
-        super || self.new
+        super || new
       end
 
       def time_left
-        self.end_date.nil? ? 0 : (self.end_date - Time.now).ceil
+        end_date.nil? ? 0 : (end_date - Time.now).ceil
       end
     end
   end
