900bc59dcf643f573ddf44a98f45ebe4e9db7353
Aleksei Khmyz <alexcrpts@gmail.com>
2024-11-04 15:41:28 +0300

    fix wording

--- db/models/models.rb
+++ db/models/models.rb
@@ -7,7 +7,7 @@ module EGS
         super
         validates_presence %i[title start_date end_date price]
         validates_unique %i[title start_date end_date]
-        errors.add(:price, 'cannot be more than 0') if price.positive?
+        errors.add(:price, 'cannot be greater than 0') if price.positive?
       end
     end
   end
