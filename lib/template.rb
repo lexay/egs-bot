f3e359987c6780a84d59ead89a0b5ec4dbdef48c
Aleksei Khmyz <alexcrpts@gmail.com>
2024-12-03 13:48:29 +0300

    refactor `Template`
    * Use a conditional instead of variable reassignment.
    * Fix '.' appearing in the header without numeration for a single game.

--- lib/template.rb
+++ lib/template.rb
@@ -5,12 +5,16 @@ module EGS
       prev_game_end_date = nil
 
       games.each_with_index do |g, i|
-        header = I18n.t(:header, start_date: stringify(g.start_date), end_date: stringify(g.end_date))
         next_game_end_date = g.end_date.to_date
-        header = '' if prev_game_end_date == next_game_end_date
-        game_idx = games.count == 1 ? '' : i + 1
-        message << header << game_idx.to_s << '. ' << describe(g)
-
+        header =
+          if prev_game_end_date == next_game_end_date
+            ''
+          else
+            I18n.t(:header, start_date: stringify(g.start_date),
+                            end_date: stringify(g.end_date))
+          end
+        game_idx = games.count == 1 ? '' : "#{i + 1}. "
+        message << header << game_idx << describe(g)
         prev_game_end_date = next_game_end_date
       end
       message << I18n.t(:banned_message)
